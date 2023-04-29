818568f97f513ed01555d20e669691e0c611aff1
timothy-b <tim@timothy-b.com>
2023-04-28 22:20:43 -0700

    Add links & logos, make a bit more mobile-friendly, add engineering principles page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,21 +14,31 @@
 
         <section class="tv-section">
             <h3>Tech</h3>
-            <p>A very brief overview: The last several years I've primarily worked on a templated ecommerce solution, checkout and payment integrations, online giving, and a social media site.</p>
+            <p>A very brief overview: The last several years I've primarily worked on
+                a <a href="https://www.logos.com/">templated</a> <a href="https://es.logos.com/">eCommerce</a> <a href="https://faithlife.com/store">solution</a>,
+                <a href="https://www.logos.com/cart">checkout</a> and payment integrations,
+                <a href="https://support.faithlife.com/hc/en-us/categories/360000498332-Faithlife-Giving">online giving</a>,
+                and <a href="https://faithlife.com/faithlife-giving/activity">a social media site.</a></p>
             <div class="tv-row">
                 <div class="tv">
                     <!-- TODO: backend image -->
                     <p class="tv-title">Backend</p>
+                    <img src="microsoft_net_logo.svg" alt="Microsoft .NET Logo" class="tv-icon tv-single-icon" />
                     <p class="tv-blurb">I have broad experience with C#, though I'm also interested in opportunities to explore other modern languages such as Go, Rust, and Scala.</p>
                 </div>
 
                 <div class="tv">
                     <p class="tv-title">Frontend</p>
+                    <img src="react_and_typescript.png" alt="React and Typescript logos" class="tv-icon" style="width: 256px;">
                     <p class="tv-blurb">I'm a fan of React + Typescript, and I'm interested in opportunities to explore other modern frontend tools such as Svelte, Elm, Ember, and Vue.</p>
                 </div>
 
                 <div class="tv">
                     <p class="tv-title">OS & Cloud</p>
+                    <div style="display: flex; align-self: center;">
+                        <img src="tux.png" alt="Linux logo" class="tv-icon tv-single-icon" />
+                        <img src="windows_logo.png" alt="Windows logo" class="tv-icon tv-single-icon" style="margin-left: -2px;" />
+                    </div>
                     <p class="tv-blurb">I have experience with Windows, Linux, Arduino, GCP, and Azure.</p>
                 </div>
             </div>
@@ -46,14 +56,17 @@
             <div class="tv-row">
                 <div class="tv">
                     <p class="tv-title">3D Printing</p>
+                    <img src="ender_5.jpg" alt="Ender 5 render" class="tv-icon" />
                     <p class="tv-blurb">Hardware, firmware, software, and drafted designs made physical. True full-stack development😄. Check out some of the stuff I've done in this domain over the last few years.</p>
                 </div>
                 <div class="tv">
                     <p class="tv-title">IOT-SEC</p>
+                    <img src="particle_photon.png" alt="A Particle Photon" class="tv-icon" />
                     <p class="tv-blurb">A DIY security system using a Raspberry Pi and a glorified ESP32.</p>
                 </div>
                 <div class="tv">
                     <p class="tv-title">DRZ-400s</p>
+                    <img src="drz_400s.png" alt="Suzuki DRZ-400s motorcycle" class="tv-icon" />
                     <p class="tv-blurb">I've taken a cheap, common dual-sport motorcycle and turned it into a lightweight adventure touring motorcycle.</p>
                 </div>
             </div>
@@ -62,12 +75,28 @@
 </div>
 
 <!-- TODO: footer w/ links & stuff -->
-<!-- TODO: omage to urbit -->
+<!-- TODO: homage to urbit -->
 
 <style>
-    
+
     section {
         padding-top: 5rem;
+    }
+
+    .tv-row {
+        display: flex;
+    }
+
+    @media (min-width: 1024px) {
+        .tv-row {
+            flex-direction: row;
+        }
+    }
+
+    @media (max-width: 1023px) {
+        .tv-row {
+            flex-direction: column;
+        }
     }
 
     .center-column-wrapper {
@@ -83,16 +112,32 @@
         max-width: 1536px;
     }
 
-    .header-tagline {
-        width: 66%;
+    @media (min-width: 512px) {
+        .header-tagline {
+            width: 66%;
+        }
+
+        section.blurb-section {
+            width: 50%;
+        }
+
+        section.tv-section {
+            width: 66%;
+        }
     }
 
-    section.blurb-section {
-        width: 50%;
-    }
+    @media (max-width: 511px) {
+        .header-tagline {
+            width: 90%;
+        }
 
-    section.tv-section {
-        width: 66%;
+        section.blurb-section {
+            width: 90%;
+        }
+
+        section.tv-section {
+            width: 80%;
+        }
     }
 
     .tv-title {
@@ -103,9 +148,14 @@
         font-size: .875rem;
     }
 
-    .tv-row {
-        display: flex;
-        flex-direction: row;
+    .tv-icon {
+        align-self: center;
+        height: 128px;
+        object-fit: contain;
+    }
+
+    .tv-single-icon {
+        width: 128px;
     }
 
     .tv {
@@ -116,6 +166,8 @@
         flex: 1 1 0%;
         background-color: rgb(var(--colors-wall-100)/var(--tw-bg-opacity));
         padding: 1rem;
+        margin: 1rem;
+        min-width: 256px;
     }
 
     .tv:not(:first-child) {
---
+++ src/routes/engineering-principles/+page.svelte
@@ -0,0 +1,14 @@
+<p>These have all had a profound impact on the way I approach software development and work relationships.</p>
+<ul>
+    <li><a href="https://echelonfront.com/extreme-ownership/">Extreme Ownership</a> by Jocko Willink</li>
+    <li><a href="https://chademeng.com/me/do-the-right-thing-wait-to-get-fired/">Do the right thing and wait to get fired</a> by Chade-Meng Tan</li>
+    <li><a href="https://mcfunley.com/choose-boring-technology">Choose boring technology</a> by Dan McKinley</li>
+    <li><a href="https://livebook.manning.com/book/functional-programming-in-scala/">Functional Programming in Scala</a> by Rúnar Bjarnason and Paul Chiusano</li>
+    <li><a href="https://echelonfront.com/dichotomy-of-leadership/">The Dichotomy of Leadership</a> by Jocko Willink</li>
+    <li><a href="https://firesomeonetoday.com/">Fire Someone Today</a> by Bob Pritchett</li>
+    <li><a href="https://www.goodreads.com/en/book/show/151881">A Failure of Nerve: Leadership in the Age of the Quick Fix</a> by Edwin H. Friedman</li>
+    <li><a href="https://startnextnow.com/">Start Next Now</a> by Bob Pritchett</li>
+    <li><a href="https://samnewman.io/books/building_microservices/">Building Microservices</a> by Sam Newman</li>
+</ul>
+
+<a href="/">back</a>
