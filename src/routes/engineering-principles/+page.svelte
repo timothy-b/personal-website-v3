6fab0a1ba7881bc193e3097b756a81b3f38158f9
Timothy Baumgartner <timmey.g.b@gmail.com>
2023-05-10 07:10:10 -0700

    Add links.

--- src/routes/engineering-principles/+page.svelte
+++ src/routes/engineering-principles/+page.svelte
@@ -5,8 +5,16 @@
 <p>These have all had a profound impact on the way I approach software development and work relationships.</p>
 <ul>
     <li><a href="https://echelonfront.com/extreme-ownership/">Extreme Ownership</a> by Jocko Willink</li>
-    <li><a href="https://chademeng.com/me/do-the-right-thing-wait-to-get-fired/">Do the right thing and wait to get fired</a> by Chade-Meng Tan</li>
-    <li><a href="https://mcfunley.com/choose-boring-technology">Choose boring technology</a> by Dan McKinley</li>
+    <li><a href="https://www.joelonsoftware.com/2000/03/21/converting-capital-into-software-that-works/">Converting Capital Into Software That Works</a> by Joel Spolsky</li>
+    <!-- TODO: sub-list this quote: "A software company has to think of recruiting the right people as its number one problem.
+        If you are successful, this can solve any other problem.
+        Hire smart people, and they will produce good stuff that you can sell and make money off.
+        Then everything else follows." -->
+    <!-- https://www.computerhope.com/issues/ch001703.htm -->
+    <li><a href="https://www.joelonsoftware.com/2000/03/23/command-and-conquer-and-the-herd-of-coconuts/">Command and Conquer and the Herd of Coconuts</a> by Joel Spolsky</li>
+    <li><a href="https://www.joelonsoftware.com/2002/02/13/the-iceberg-secret-revealed/">The Iceberg Secret, Revealed</a> by Joel Spolsky</li>
+    <li><a href="https://chademeng.com/me/do-the-right-thing-wait-to-get-fired/">Do the Right Thing and Wait to Get Fired</a> by Chade-Meng Tan</li>
+    <li><a href="https://mcfunley.com/choose-boring-technology">Choose Boring Technology</a> by Dan McKinley</li>
     <li><a href="https://livebook.manning.com/book/functional-programming-in-scala/">Functional Programming in Scala</a> by Rúnar Bjarnason and Paul Chiusano</li>
     <li><a href="https://echelonfront.com/dichotomy-of-leadership/">The Dichotomy of Leadership</a> by Jocko Willink</li>
     <li><a href="https://firesomeonetoday.com/">Fire Someone Today</a> by Bob Pritchett</li>
